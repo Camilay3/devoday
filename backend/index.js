33383dd3031c24986aeeb741a04f5283734126c1
Camila Santiago <camila.santiago026@gmail.com>
2026-09-08 14:07:38 -0300

    chore: Swagger

--- backend/index.js
+++ backend/index.js
@@ -1,4 +1,15 @@
 import express from "express";
+import swaggerUi from 'swagger-ui-express';
+import YAML from 'yamljs';
 
 const app = express();
 app.listen(3003, () => console.log('Servidor iniciado!'));
+
+const swaggerDocument = YAML.load('./swagger.yaml');
+app.use(express.json())
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
+app.post('/auth/cadastro', (req, res) => {
+	console.log(req.body);
+	res.status(201).json({ message: 'Cadastro recebido' });
+});
